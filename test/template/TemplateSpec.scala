e13f3ea5b8cee094eef1eb12d464722217681300
Benjamin Trenker <benjamin.trenker@gmail.com>
2016-01-26 18:53:20 +0100

    #9 improved tests

--- test/template/TemplateSpec.scala
+++ test/template/TemplateSpec.scala
@@ -1,5 +1,7 @@
 package template
 
+import java.util.UUID
+
 import play.api.libs.json.{JsObject, JsString}
 import play.api.test.{PlaySpecification, _}
 
@@ -10,13 +12,15 @@ object TemplateSpec extends PlaySpecification {
   "Template" should {
 
     "return status OK if the template exists" in new WithApplication(fakeApplication) {
-      val Some(result) = route(FakeRequest(POST, "/test.mustache"))
+      val requestBody = JsObject(List(("name", JsString(UUID.randomUUID().toString))))
+      val Some(result) = route(FakeRequest(POST, "/test.mustache").withJsonBody(requestBody))
 
       status(result) must equalTo(OK)
     }
 
     "return status NOT_FOUND if no valid template with requested name exists" in new WithApplication(fakeApplication) {
-      val Some(result) = route(FakeRequest(POST, "/notexists.mustache"))
+      val requestBody = JsObject(List(("name", JsString(UUID.randomUUID().toString))))
+      val Some(result) = route(FakeRequest(POST, "/notexists.mustache").withJsonBody(requestBody))
 
       status(result) must equalTo(NOT_FOUND)
     }
